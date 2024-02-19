f060d5b5a0cdca93902fc8b8f6bca14953131736
Lovenish <heelme1181@gmail.com>
2024-02-19 19:50:55 +0530

    add functionality to the sign up page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,46 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill out all fields')
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json()
+      if (data.success === false){
+        return setErrorMessage(data.message);
+      }
+      setLoading(false)
+      if(res.ok){
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -13,7 +51,7 @@ export default function Signup() {
             className="text-4xl sm:text-xl font-bold dark:text-white"
           >
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 rounded-lg text-white">
-              Sahand's
+              Lovenish's
             </span>
             Blog
           </Link>
@@ -25,28 +63,60 @@ export default function Signup() {
 
         {/* Right */}
         <div className="flex-1 ">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your Username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your email" />
-              <TextInput type="text" placeholder="Email" id="email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your Password" />
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign up
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <>
+                  <Spinner size='sm' />
+                  <span className="pl-3">Loading ....</span>
+                  </>
+                ) : 'Sign up'
+              }
+              
             </Button>
           </form>
- 
+
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an Account ?</span>
-            <Link to="/sign-in" className="text-blue-500">Sign in</Link>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign in
+            </Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className="mt-5" color='failure' >
+                {errorMessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
